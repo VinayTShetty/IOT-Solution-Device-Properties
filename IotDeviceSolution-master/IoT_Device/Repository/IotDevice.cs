2edf1ac67f9acdee288b90c347cb86ac9b1ad52b
VinayTS <vinaytshetty@gmail.com>
2023-04-13 06:25:42 +0000

    first commit

---
+++ IotDeviceSolution-master/IoT_Device/Repository/IotDevice.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+
+namespace IotHubDevice.Repository
+{
+    public class IotDevice
+    {
+        public static RegistryManager registryManager;
+
+        private static string iothubConnection = "HostName=COURSEIOTHUBDEMO.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=z6j5aAhwoUFnme7OVB4XGYTCtMKkYpW4zHMdY2wdoz4=\n";
+
+        public  static async Task AddIotDevice(string deviceName)
+        {
+            Device device;
+            if(string.IsNullOrEmpty(deviceName))
+            {
+                throw new ArgumentNullException("Kindly Add Device Name:");
+            }
+            registryManager= RegistryManager.CreateFromConnectionString(iothubConnection);
+            device = await registryManager.AddDeviceAsync(new Device(deviceName));
+            
+        }
+
+        public static async Task<Device> GetIotDevice(string deviceId)
+        {
+            Device device;
+            registryManager=RegistryManager.CreateFromConnectionString(iothubConnection);
+            device= await registryManager.GetDeviceAsync(deviceId);
+            return device;
+        }
+
+        public  static async Task<Device> UpdateIotDevice(string deviceId)
+        {
+            if(string.IsNullOrEmpty(deviceId))
+            {
+                throw new ArgumentNullException("Kindly Enter Device Id:");
+            }
+            Device device =  new Device(deviceId);
+            registryManager = RegistryManager.CreateFromConnectionString(iothubConnection);
+            device = await registryManager.GetDeviceAsync(deviceId);
+            device.StatusReason = "Device Updated";
+            device = await registryManager.UpdateDeviceAsync(device);
+            return device;
+
+        }
+
+        public static async Task DeleteDevice(string deviceId)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(iothubConnection);
+            await registryManager.RemoveDeviceAsync(deviceId);
+        }
+    }
+}
